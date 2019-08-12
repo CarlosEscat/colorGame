9d52817891648a195229cb82ec2437f73a5485c4
CarlosEscat <carlosescat@gmail.com>
2019-08-12 16:40:17 +0200

    Folders for models and login

---
+++ user/model.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const User = db.define("user", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  points: Sequelize.INTEGER,
+  guessed: Sequelize.BOOLEAN
+});
+
+module.exports = User;
