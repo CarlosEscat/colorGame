10a7a0593beeef6796f24ad901aa8dfec5028938
CarlosEscat <carlosescat@gmail.com>
2019-08-13 16:01:30 +0200

    commit branch develop

--- room/model.js
+++ room/model.js
@@ -1,21 +1,18 @@
-// const Sequelize = require("sequelize");
-// const db = require("../db");
-// const User = require("../user/model");
+const Sequelize = require("sequelize");
+const db = require("../db");
+const User = require("../user/model");
 
-
-const Room = db.define(
-  "room", {
-    //roomName: Sequelize.STRING,
-    word: Sequelize.STRING,
-    color: Sequelize.STRING,
-    status: { type: Sequelize.STRING, defaultValue: "joinning" },
-    round: { type: Sequelize.INTEGER, defaultValue: 0 }
-  });
+const Room = db.define("room", {
+  //roomName: Sequelize.STRING,
+  word: Sequelize.STRING,
+  color: Sequelize.STRING,
+  status: { type: Sequelize.STRING, defaultValue: "joinning" },
+  round: { type: Sequelize.INTEGER, defaultValue: 0 }
+});
 
 User.belongsTo(Room);
 Room.hasMany(User);
 //console.log('Room.users test', Room.Users)
 //Room.Users = [{ name, password, points }];
 
-
-// module.exports = Room;
+module.exports = Room;
